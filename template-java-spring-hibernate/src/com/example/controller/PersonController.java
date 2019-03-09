d687bcb7ee88c961ef1cca01c3c2e2a32e4061ea
j4sysiak <j4sysiak@gmail.com>
2019-03-09 07:55:14 +0100

    1

---
+++ template-java-spring-hibernate/src/com/example/controller/PersonController.java
@@ -0,0 +1,38 @@
+package com.example.controller;
+
+import java.util.Map;
+
+import com.example.model.Person;
+import com.example.service.PersonService;
+
+@Controller
+public class PersonController {
+
+    @Autowired
+    private PersonService personService;
+
+    @RequestMapping("/")
+    public String listPeople(Map<String, Object> map) {
+
+        map.put("person", new Person());
+        map.put("peopleList", personService.listPeople());
+
+        return "people";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String addPerson(@ModelAttribute("person") Person person, BindingResult result) {
+
+        personService.addPerson(person);
+
+        return "redirect:/people/";
+    }
+
+    @RequestMapping("/delete/{personId}")
+    public String deletePerson(@PathVariable("personId") Integer personId) {
+
+        personService.removePerson(personId);
+
+        return "redirect:/people/";
+    }
+}
